9e34c4608106c8f6fad4d9cbd2d9f360b8db01a1
matheustakasaki <matheus8antunes@gmail.com>
2022-05-28 15:23:18 -0300

    Criando Validação do valor de cpf e exibindo mensagem de sucesso ou erro

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 console.log('Javascript carregado');
 
-function handleValidacao() {
-    console.log('Iniciando validação cpf')
+function handleValidaCpf(cpf) {
+
+    return cpf
 }
+
+function handleValidacao() {
+    console.log('Iniciando validação cpf');
+
+    var cpf = document.getElementById('cpf').value;
+
+    var resultadoValidacao = handleValidaCpf(cpf);
+    console.log(cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block'
+    } else {
+        document.getElementById('error').style.display = 'block'
+    }
+}
+
