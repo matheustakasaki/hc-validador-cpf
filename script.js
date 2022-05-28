fdb4f66bae79b4833a62f754af388375f6baf42c
matheustakasaki <matheus8antunes@gmail.com>
2022-05-28 15:55:20 -0300

    Criando Validação do primeiro dígito

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ console.log('Javascript carregado');
 
 function handleValidaCpf(cpf) {
 
+    if (cpf.length != 11) {
+        return false
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9)
+
+
+        var soma = 0
+        for (let i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i
+        }
+        console.log(soma);
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11)
+
+        // Validação do primeiro dígito
+        if (resultado != digitos.charAt(0)) {
+            return false
+        }
+        console.log(digitos.toString().charAt(0) + ' é a primeira posição da variável soma')
+    }
     return cpf
 }
 
@@ -11,7 +32,6 @@ function handleValidacao() {
     var cpf = document.getElementById('cpf').value;
 
     var resultadoValidacao = handleValidaCpf(cpf);
-    console.log(cpf);
 
     if (resultadoValidacao) {
         document.getElementById('success').style.display = 'block'
